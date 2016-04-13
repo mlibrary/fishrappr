7eed66da63beef6e68ac4f8bc45506867ef051d6
Nabeela Jaffer <njaffer@umich.edu>
2016-04-13 15:49:54 -0400

    Add Migrations

---
+++ db/migrate/20160413202246_add_issues.rb
@@ -0,0 +1,22 @@
+# issues 
+class AddIssues < ActiveRecord::Migration
+  def self.up      
+    create_table :issues do |t|
+      t.integer :issue_id
+      t.text  :hathitrust
+      t.integer :volume
+      t.integer :issue_no
+      t.integer :edition
+      t.string :dateIssued
+      t.string :newspaper
+
+      t.timestamps null: false
+    end
+
+    add_index :issues, :issue_id
+  end
+
+  def self.down
+    drop_table :issues
+  end
+end
