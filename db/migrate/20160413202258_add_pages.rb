bfb3cdac8753ad5710b52309fa860fec99ed9658
Gordon Leacock <gordonl@umich.edu>
2016-04-20 15:03:54 -0400

    update migrations

--- db/migrate/20160413202258_add_pages.rb
+++ db/migrate/20160413202258_add_pages.rb
@@ -1,17 +1,17 @@
 class AddPages < ActiveRecord::Migration
   def self.up
     create_table :pages do |t|
-      t.integer  :page_id
-      t.integer  :page_no
-      t.integer  :page_order
-      t.integer  :issue_no
+      t.integer  :issue_id
+      t.string   :page_no
+      t.string   :page_order
+      t.string   :issue_no
       t.string   :text_link
       t.string   :img_link
 
       t.timestamps null: false
     end
 
-    add_index :pages, :page_id
+    add_index :pages, :id
   end
 
   def self.down
