20ec59dfa62d732ba4b22b69536d660054843b87
Nabeela Jaffer <njaffer@umich.edu>
2016-04-25 10:59:45 -0400

    one more

--- db/migrate/20160413202246_add_issues.rb
+++ db/migrate/20160413202246_add_issues.rb
@@ -2,13 +2,13 @@
 class AddIssues < ActiveRecord::Migration
   def self.up      
     create_table :issues do |t|
-      t.string  :hathitrust
-      t.string  :volume
-      t.string  :issue_no
-      t.string  :edition
-      t.string  :date_issued
-      t.string  :newspaper
-      t.integer :pages_count # track page associated with this issue
+      t.string  :hathitrust  #hathitrust id
+      t.string  :volume      #volume number
+      t.string  :issue_no    #issue number
+      t.string  :edition     #edition number
+      t.string  :date_issued #date issued
+      t.string  :newspaper   #newspaper title
+      t.integer :pages_count #track page associated with this issue
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -42,8 +42,7 @@ ActiveRecord::Schema.define(version: 20160413202258) do
   create_table "pages", force: :cascade do |t|
     t.integer  "issue_id"
     t.string   "page_no"
-    t.string   "page_order"
-    t.string   "issue_no"
+    t.integer  "sequence"
     t.string   "text_link"
     t.string   "img_link"
     t.datetime "created_at", null: false
