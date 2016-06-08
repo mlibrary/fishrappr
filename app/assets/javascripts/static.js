2de7eb1a5ec4f64ba0dab1cdf8a3d463f29f177a
Gordon Leacock <gordonl@umich.edu>
2016-06-08 11:29:56 -0400

    add rotating wait icon to search button

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+// Replaces the magnifying glass in the search button with a rotating wait icon
+// See changes in customers.css.scss too, as well as /catalog and /home html.
+$(document).on("click",'button#search',function(){
+  $("div.search span#not-searching").addClass("hidden");
+  $("div.search span#now-searching").removeClass("hidden");
+  $("body").css("cursor", "progress");
+});
