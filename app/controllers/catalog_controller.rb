a096aad52061fde337515710d2a9b6761e7d81e4
Gordon Leacock <gordonl@umich.edu>
2016-11-09 15:18:54 -0500

    Add Front Page Only facet

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -145,15 +145,17 @@ class CatalogController < ApplicationController
     #                      maxlength: 4
     #                    }
     # # seg was 6, len was 8
+    # config.add_facet_field 'issue_sequence', single: true
 
+    config.add_facet_field 'issue_sequence_field', label: 'Only Front Pages', query: {
+        issue_sequence_1: { label: 'Select only front pages', fq: 'issue_sequence:1' }
+    }
     config.add_facet_field 'date_issued_yyyy10_ti', label: 'Decade', sort: 'index', limit: 20, collapse: false
     config.add_facet_field 'date_issued_yyyy_ti', label: 'Year', sort: 'index', limit: 20
     config.add_facet_field 'date_issued_mm_ti', label: 'Month', sort: 'index', helper_method: :get_month_label
     config.add_facet_field 'date_issued_dd_ti', label: 'Day', sort: 'index', limit: 20
     config.add_facet_field 'date_issued_yyyymmdd_ti', label: 'Date', show: false
-    config.add_facet_field 'issue_sequence_field', label: 'First Page', query: {
-        issue_sequence_1: { label: 'First Page', fq: 'issue_sequence:1' }
-    }
+
     #config.add_facet_field 'example_pivot_field', label: 'Pivot Field', :pivot => ['format', 'language_facet']
 
     #config.add_facet_field 'example_query_facet_field', label: 'Date', :query => {
