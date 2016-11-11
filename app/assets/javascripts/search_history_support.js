c4bef7a14f447b12cce7b732ff7423657c41584d
Roger Espinosa <roger@umich.edu>
2016-11-11 16:25:10 -0500

    session-less support works across more pages in the window/tab

--- app/assets/javascripts/search_history_support.js
+++ app/assets/javascripts/search_history_support.js
@@ -1,20 +1,25 @@
 $().ready(function() {
 
-  if ( $("body").is(".blacklight-catalog-index") || $("body").is(".blacklight-catalog-browse") ) {
+  var action = location.pathname ? location.pathname.substr(1) : null;
+  if ( action == 'index' || action == 'browse' ) {
     var query = location.search.substr(1);
-    if ( query ) {
-      var params = $.deparam(query)
-      params.controller = 'catalog';
-      params.action = location.pathname.substr(1);
-      sessionStorage.setItem('query_params', JSON.stringify(params));
-    }
+    var params = query ? $.deparam(query) : {};
+    params.controller = 'catalog';
+    params.action = action;
+    sessionStorage.setItem('query_params', JSON.stringify(params));
   }
 
-  $("body").on('click', 'a[href^="/midaily"]', function() {
+  var set_search_cookie = function() {
     var q = sessionStorage.getItem('query_params');
     if ( q ) {
       Cookies.set('query_params', q, { expires: 1 }); // already JSON
-    }
+    }    
+  }
+
+  // /search and /browse will ignore the cookie, but otherwise
+  // your search session works all over the app
+  $(window).on('beforeunload', function(e) {
+    set_search_cookie();
   })
 
   // delete the cookie here; for some reason cookie.delete from Rails
