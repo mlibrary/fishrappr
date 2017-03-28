8e620884e391fba441956432b6c948fad5375eb7
respinos <roger@umich.edu>
2017-03-28 14:35:48 -0400

    Advanced search document highlighting (#292)
    * linkify regents
    
    * truly use the advanced search parser when fetching highlights
    
    * fix search result highlighting for wildcard

--- app/models/document_search_builder.rb
+++ app/models/document_search_builder.rb
@@ -1,8 +1,7 @@
 class DocumentSearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
   include BlacklightAdvancedSearch::AdvancedSearchBuilder
-  self.default_processor_chain += [:add_advanced_parse_q_to_solr, :add_advanced_search_to_solr, :modify_frag_size, :modify_fl ]
-  include BlacklightRangeLimit::RangeLimitBuilder
+  self.default_processor_chain += [:add_advanced_parse_q_to_solr, :add_advanced_search_to_solr, :modify_frag_size, :modify_fl, :filter_by_page_identifier ]
 
   def modify_frag_size(solr_parameters)
     solr_parameters[:"hl.fragsize"] = 0
@@ -11,4 +10,13 @@ class DocumentSearchBuilder < Blacklight::SearchBuilder
   def modify_fl(solr_parameters)
     solr_parameters[:fl] = '*'
   end
+
+  def filter_by_page_identifier(solr_parameters)
+    solr_parameters[:fq] ||= []
+    page_identifier = blacklight_params[:page_identifier]
+    if page_identifier
+      solr_parameters[:fq] << "{!term f=id}#{page_identifier}"
+    end
+  end
+
 end
