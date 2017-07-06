09df22b35aea1fc515daedfaee9244274cc7d19b
Roger Espinosa <roger@umich.edu>
2017-07-06 18:17:10 -0400

    fix conflicts

--- lib/devise/strategies/http_header_authenticatable.rb
+++ lib/devise/strategies/http_header_authenticatable.rb
@@ -21,11 +21,6 @@ module Devise
            # u.populate_attributes
           end
           success!(u)
-          #if u.nil?
-          #  u = User.create(email: user)
-          #  u.populate_attributes
-          #end
-          #success!(u)
         else
           fail!
         end
